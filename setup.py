e4d1745652a1737b6f9281b3340cce9b75cf8203
3e8 <info@3e8.ch>
2020-07-21 18:49:17 +0200

    complete setup

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 setup(
-  name = 'simpleimage',         # How you named your package folder (MyLib)
-  packages = ['simpleimage'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Simple image API for educational purposes',   # Give a short description about your library
-  author = '3e8',                   # Type in your name
-  author_email = 'info@3e8.ch',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'simpleimage',
+  packages = ['simpleimage'],
+  version = '0.1',
+  license='MIT',
+  description = 'Simple image API for educational purposes',
+  author = '3e8',
+  author_email = 'info@3e8.ch',
+  url = 'https://github.com/adilu/simpleimage',
+  download_url = 'https://github.com/adilu/simpleimage/archive/v_01.tar.gz',
+  keywords = ['Image', 'python', 'educational'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
